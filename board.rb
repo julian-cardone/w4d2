cf464de94412a0644d4898ff99d5b1a3c1e4eb9b
Julian Cardone <juliancardone1@gmail.com>
2022-10-04 14:37:14 -0400

    finished move piece method

--- board.rb
+++ board.rb
@@ -1,33 +1,68 @@
+require_relative 'piece.rb'
+
 class Board
 
     def initialize
         @rows = Array.new(8){ Array.new(8) }
-        @null_piece = NullPiece.new
+        #@null_piece = NullPiece.new
         self.set_up_pieces
         self.set_up_null_pieces
     end
 
-    private
     def set_up_pieces
-        queen_pos = []
-        rook_pos = []
-        knight_pos = []
-        queen_pos = []
-        rook_pos = []
-        knight_pos = []
-        self[queen_pos] = Queen.new
-        self[rook_pos] = Rook.new
-        self[knight_pos] = Knight.new
-        self[queen_pos] = Queen.new
-        self[rook_pos] = Rook.new
-        self[knight_pos] = Knight.new
+        # queen_pos = []
+        # rook_pos = []
+        # knight_pos = []
+        # queen_pos = []
+        # rook_pos = []
+        # knight_pos = []
+        # self[queen_pos] = Queen.new
+        # self[rook_pos] = Rook.new
+        # self[knight_pos] = Knight.new
+        # self[queen_pos] = Queen.new
+        # self[rook_pos] = Rook.new
+        # self[knight_pos] = Knight.new
+        white_queen = Piece.new
+        black_queen = Piece.new
+
+        black_knight_1 = Piece.new
+        black_knight_2 = Piece.new
+        white_knight_1 = Piece.new
+        white_knight_2 = Piece.new
+
+        black_rook_1 = Piece.new
+        black_rook_2 = Piece.new
+        white_rook_1 = Piece.new
+        white_rook_2 = Piece.new
+
+
+        self[[0,3]] = black_queen
+        self[[7,3]] = white_queen
+
+        self[[0,1]] = black_knight_1
+        self[[0,6]] = black_knight_2
+        self[[7,1]] = white_knight_1
+        self[[7,6]] = white_knight_2
+
+        self[[0,0]] = black_rook_1
+        self[[0,7]] = black_rook_2
+        self[[7,0]] = white_rook_1
+        self[[7,7]] = white_rook_2
+
     end
 
-    private
     def set_up_null_pieces
-        null_pieces_positions = []
-        null_pieces_positions.each do |pos|
-            self[pos] = NullPiece.new
+        # null_pieces_positions = []
+        # null_pieces_positions.each do |pos|
+        #     #self[pos] = NullPiece.new
+        #     self[pos] = nil
+
+        # end
+        (2..5).each do |i|
+            (0..7).each do |j|
+                pos = [i,j]
+                self[pos] = nil
+            end
         end
     end
 
@@ -41,17 +76,21 @@ class Board
         @rows[row][col] = val
     end
 
-    def move_piece(color, start_pos, end_pos)
+    def move_piece(start_pos, end_pos)
         piece = self[start_pos]
-        unless piece.is_a(NullPiece)
-            moves = piece.moves
-            end_positions = moves.map { |move| process(start_pos, move) }
-            if end_positions.include?(end_pos)
-                self[end_pos] = piece
-            else
-                raise "Can not move to #{end_pos}"
-            end
+        if piece == nil
+             raise "exception"
+        else
+            self[end_pos] = piece
+            self[start_pos] = nil
         end
+            # moves = piece.moves
+            # end_positions = moves.map { |move| process(start_pos, move) }
+            # if end_positions.include?(end_pos)
+            #     self[end_pos] = piece
+            # else
+            #     raise "Can not move to #{end_pos}"
+            # end
     end
 
     def valid_pos?(pos)
@@ -84,7 +123,7 @@ class Board
     end
 
     def move_piece!(color, start_pos, end_pos)
-        
+
     end
 
 end
